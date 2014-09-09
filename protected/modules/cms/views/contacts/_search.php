5d90b75d7e76b3c8f7eb181dd50237467a1bdca1
Harutyun <harutmargaryan91@gmail.com>
2014-09-09 15:41:38 +0400

    save7

---
+++ protected/modules/cms/views/contacts/_search.php
@@ -0,0 +1,80 @@
+<?php
+/* @var $this ContactsController */
+/* @var $model Contacts */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'address_ru'); ?>
+		<?php echo $form->textArea($model,'address_ru',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'address_en'); ?>
+		<?php echo $form->textArea($model,'address_en',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'address_coord_x'); ?>
+		<?php echo $form->textField($model,'address_coord_x',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'address_coord_y'); ?>
+		<?php echo $form->textField($model,'address_coord_y',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'worktime'); ?>
+		<?php echo $form->textField($model,'worktime',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'phone'); ?>
+		<?php echo $form->textArea($model,'phone',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'contact_email'); ?>
+		<?php echo $form->textField($model,'contact_email',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'fb_link'); ?>
+		<?php echo $form->textField($model,'fb_link',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'in_link'); ?>
+		<?php echo $form->textField($model,'in_link',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'pin_link'); ?>
+		<?php echo $form->textField($model,'pin_link',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'feedback_email'); ?>
+		<?php echo $form->textField($model,'feedback_email',array('size'=>60,'maxlength'=>250)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
