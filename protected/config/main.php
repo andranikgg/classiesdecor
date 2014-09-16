9249e118b038e1637e1a785bf6789a99d8e5f6e2
andranik.zurabyan <zurabjan@gmail.com>
2014-09-16 17:42:40 +0400

    kkkkkk

--- protected/config/main.php
+++ protected/config/main.php
@@ -83,13 +83,13 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),*/
 		// release database
-		/*'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=khach2_classies',
-			'emulatePrepare' => true,
-			'username' => 'khach2_classies',
-			'password' => 'Uranida060292',
-			'charset' => 'utf8',
-		),*/
+//		'db'=>array(
+//			'connectionString' => 'mysql:host=localhost;dbname=khach2_classies',
+//			'emulatePrepare' => true,
+//			'username' => 'khach2_classies',
+//			'password' => 'Uranida060292',
+//			'charset' => 'utf8',
+//		),
 
         // local database
         'db'=>array(
